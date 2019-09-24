f461797dafd1a2ea4b58b247da73d59bac8c552e
朱时岗 <wx_07fc2aba38fd42e6b139380b3f208e8d@git.code.tencent.com>
2019-09-24 20:15:21 +0800

    修改命令行模式以及配置迁移app.conf

--- config/addons.php
+++ config/addons.php
@@ -4,8 +4,16 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'testhook' => 
+    array (
+      0 => 'test',
+    ),
   ),
   'route' => 
   array (
+    '/example$' => 'example/index/index',
+    '/example/d/[:name]' => 'example/demo/index',
+    '/example/d1/[:name]' => 'example/demo/demo1',
+    '/example/d2/[:name]' => 'example/demo/demo2',
   ),
 );
