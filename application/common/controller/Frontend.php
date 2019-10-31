4678ead15d9915fc204644ca7a4e6b45d369db69
helloworldzsg <46045921+hellowzsg@users.noreply.github.com>
2019-10-31 19:26:07 +0800

    修改 批量配置方式

--- application/common/controller/Frontend.php
+++ application/common/controller/Frontend.php
@@ -107,7 +107,7 @@ class Frontend extends Controller
         ];
         $config = array_merge($config, Config::get("template.tpl_replace_string"));
 
-        Config::set('upload', array_merge(Config::get('upload.'), $upload));
+        Config::set(array_merge(Config::get('upload.'), $upload), 'upload');
         // 配置信息后
         $config = (object) $config;
         Hook::listen("config_init", $config);
